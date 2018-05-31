c69a0cc0e44161c6dc6c78d29426e6d6d55b1c59
vikas9599 <vikasrathour012@gmail.com>
2018-05-31 15:57:55 +0530

    Project Set Up

---
+++ Mastram App/Libs/TempTestCase1527758094266.groovy
@@ -0,0 +1,21 @@
+import com.kms.katalon.core.main.TestCaseMain
+import com.kms.katalon.core.logging.KeywordLogger
+import groovy.lang.MissingPropertyException
+import com.kms.katalon.core.testcase.TestCaseBinding
+import com.kms.katalon.core.driver.internal.DriverCleanerCollector
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.configuration.RunConfiguration
+import com.kms.katalon.core.webui.contribution.WebUiDriverCleaner
+import com.kms.katalon.core.mobile.contribution.MobileDriverCleaner
+
+
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.webui.contribution.WebUiDriverCleaner())
+DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.mobile.contribution.MobileDriverCleaner())
+
+
+RunConfiguration.setExecutionSettingFile('C:\\Users\\Vikash\\AppData\\Local\\Temp\\Katalon\\Test Cases\\App Launch\\20180531_144454\\execution.properties')
+
+TestCaseMain.beforeStart()
+
+        TestCaseMain.runTestCase('Test Cases/App Launch', new TestCaseBinding('Test Cases/App Launch', [:]), FailureHandling.STOP_ON_FAILURE , false)
+    
